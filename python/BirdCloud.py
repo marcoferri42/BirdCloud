7eb85d659b2c21c6d5d18bfe8f7d1377701739a3
AF82222 <marco.ferri@lynxspa.com>
2022-10-09 00:42:02 +0200

    mostly formatting

--- python/BirdCloud.py
+++ python/BirdCloud.py
@@ -10,81 +10,106 @@ from math import cos, sin
 from tkinter import *
 import time
 
+# xIncrement, yIncrement =np.loadtxt('coordBirds.dat',unpack=True, usecols=(0,1,2)) #Load from C
+# subprocess.call(["gcc",cmd]) #Compiling C
+# subprocess.call("./a.out") #Run C
 
+# Varaibles
 cmd = "birds.c"
-nBirds = 2
+nBirds = 1
 displayedBirds = []
-screenWidth = 800 
+screenWidth = 800
 screenHeight = 800
 birdWidth = 5
 birdHeight = 5
 xIncrement = 4
 yIncrement = 2
 maxVel = xIncrement + yIncrement
-initialX = screenWidth/2
-initialY = screenHeight/2
+initialX = screenWidth / 2
+initialY = screenHeight / 2
 x = initialX
 y = initialY
-borderEvent = False
-
-#xIncrement, yIncrement =np.loadtxt('coordBirds.dat',unpack=True, usecols=(0,1,2)) #Load from C
-
-#subprocess.call(["gcc",cmd]) #Compiling C
-#subprocess.call("./a.out") #Run C
-
-#Setting up display
+# Setting up display
 gui = Tk()
 gui.geometry("800x800")
 gui.title("BirdCloud")
-canvas = Canvas(gui, width=800, height=800, bg='white')
+canvas = Canvas(gui, width=screenWidth, height=screenHeight, bg="white")
 canvas.pack()
 frame = 0
 
 # Changes direction
 def incrementChange(param):
     global xIncrement, yIncrement
-    if(param =="x"):
+    if param == "x":
         xIncrement = -xIncrement
     else:
         yIncrement = -yIncrement
 
+
 # Calculates new coordinates
 def calculateCoords():
     global x, y, xIncrement, yIncrement
-    x = abs(canvas.coords(displayedBirds[i])[0]) + xIncrement -1
-    y = abs(canvas.coords(displayedBirds[i])[1]) + yIncrement -1
+    x = abs(canvas.coords(displayedBirds[i])[0]) + xIncrement - 1
+    y = abs(canvas.coords(displayedBirds[i])[1]) + yIncrement - 1
 
-
-
-while True :
-#stop looping for frame 
+# Main loop
+while True:
+    # Frame loop
     for i in range(nBirds):
-        if(frame == 0):
+        if frame == 0:
             print("|----simulation-start----|")
-            displayedBirds.append(canvas.create_rectangle(0, 0, birdWidth, birdHeight, fill='black'))
+            displayedBirds.append(
+                canvas.create_rectangle(0, 0, birdWidth, birdHeight, fill="black")
+            )
             canvas.moveto(displayedBirds[i], initialX, initialY)
 
-            x = abs(canvas.coords(displayedBirds[i])[0]) 
+            x = abs(canvas.coords(displayedBirds[i])[0])
             y = abs(canvas.coords(displayedBirds[i])[1])
-            displayText = canvas.create_text(screenWidth/2, 12.5, font="Monospace 10 bold", text="x: " + str(x) + " y: " + str(y) + "    increments: (" + str(xIncrement) + "," + str(yIncrement) + ")" + "  frame:" + str(frame))
+            displayText = canvas.create_text(
+                screenWidth / 2,
+                12.5,
+                font="Monospace 10 bold",
+                text="x: "
+                + str(x)
+                + " y: "
+                + str(y)
+                + "    increments: "
+                + str(xIncrement)
+                + ","
+                + str(yIncrement)
+                + "  frame:"
+                + str(frame),
+            )
 
-        # corner checking + errorThrow //// TODO: NOT WORKING
-        if ( x+xIncrement >= screenWidth or  x+xIncrement <= 0 ):
+        # Border checking
+        if x + xIncrement >= screenWidth or x + xIncrement <= 0:
             print("|----x-asxis-bordercollision----|")
             incrementChange("x")
             calculateCoords()
-      
-        if( y+yIncrement >= screenHeight or  y+yIncrement <= 0):
+
+        if y + yIncrement >= screenHeight or y + yIncrement <= 0:
             print("|----y-axis-bordercollision----|")
             incrementChange("y")
             calculateCoords()
         else:
             calculateCoords()
 
-        # display of object w new coords
+        # Display of object w new coords
         canvas.moveto(displayedBirds[i], x, y)
-        canvas.itemconfigure(displayText, text="x: " + str(x) + " y: " + str(y) + "    increments: (" + str(xIncrement) + "," + str(yIncrement) + ")" + "  frame:" + str(frame))
-     
-    frame = 1
+        canvas.itemconfigure(
+            displayText,
+            text="x: "
+            + str(x)
+            + " y: "
+            + str(y)
+            + "    increments: "
+            + str(xIncrement)
+            + ","
+            + str(yIncrement)
+            + "  frame:"
+            + str(frame),
+        )
+
+    frame = frame+1
     gui.update()
     time.sleep(0.001)
